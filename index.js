a17175208d64055d29c8b909cca25cf4183431b6
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-03-15 23:47:35 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const express = require('express');
+const { Server } = require('socket.io');
+const http = require('http');
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', (socket) => {
+  console.log('user connected');
+});
+
+server.listen(3000, () => {
+  console.log('server running...');
+});
