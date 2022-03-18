9eb7d1b38d73db546689d50c930d5ae62f82e183
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-03-18 23:15:24 +0000

    receive event from client on a different domain

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const { Server } = require('socket.io');
 const http = require('http');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://127.0.0.1:5500',
+    allowedHeaders: ['my-custom-header'],
+    credentials: true,
+  },
+});
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
@@ -12,8 +19,12 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('user connected');
+
+  socket.on('chat-message', (msg) => {
+    console.log(msg);
+  });
 });
 
 server.listen(3000, () => {
-  console.log('server running..');
+  console.log('server running...');
 });
