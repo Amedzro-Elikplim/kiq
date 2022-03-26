f3f514d1591cbc4785c6be25a2be07e78dc46ec8
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-03-26 20:22:31 +0000

    Run npm install

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ const express = require('express');
 const { Server } = require('socket.io');
 const http = require('http');
 const config = require('config');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: config.get('URL'),
-    allowedHeaders: ['my-custom-header'],
     credentials: true,
   },
 });
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.send('Hello world');
 });
 
 io.on('connection', (socket) => {
